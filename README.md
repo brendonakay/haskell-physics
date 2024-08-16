d1e481b3193835627287827bf678e22acbd53fd2
Brendon A. Kay <bak04280@gmail.com>
2024-08-15 19:25:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TODO
+- [ ] Combine MacOS and Linux flakes
---
+++ app/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Graphics.Gnuplot.Simple
+
+type R = Double
+
+square :: R -> R
+square x = x ** 2
+
+plot1 :: IO ()
+plot1 = plotFunc [] [-3, 2.99 .. 3] square
+
+main :: IO ()
+main = plot1
