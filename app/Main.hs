ec31e3ac8e3e39262bab8d51534cfdad3ee4d77e
Brendon A. Kay <bak04280@gmail.com>
2024-08-16 21:33:23 -0600

    Working plot gen

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,12 @@ type R = Double
 square :: R -> R
 square x = x ** 2
 
+-- Ch7
 plot1 :: IO ()
 plot1 = plotFunc [] [-3, 2.99 .. 3] square
 
 main :: IO ()
-main = plot1
+main = do
+  plot1
+  _ <- getLine
+  return ()
